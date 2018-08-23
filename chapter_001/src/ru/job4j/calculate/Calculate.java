ec0f99e36966d5897504d5d2d0953e177cb2d301
wizardofooz <mailto:wizardofooz666@gmail.com>
2018-08-23 15:36:21 +0300

    1.3 JavaDoc. Code Convention

--- chapter_001/src/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j.calculate;
+/** 
+ *Calculate	
+ * @author Maxim Bardakov (wizardofooz666@gmail.com)
+ */
 public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */	
     public static void main(String[] args)
     {
         System.out.print("Hello World!");
