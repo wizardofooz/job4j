2421bc60a112369e7999b17dde4afb1f581a4e79
wizardofooz <mailto:wizardofooz666@gmail.com>
2018-09-02 21:22:57 +0300

    6.4. Слово начинается с ...

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author Maxim Bardakov (mailto:wizardofooz666@gmail.com
+ * @version $Id$
+ * @since 02.09.2018
+ */
+public class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+}
